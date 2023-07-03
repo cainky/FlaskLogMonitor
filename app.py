5ccbdcd9c0cae6acea6f2879fe26666662388682
cainky <kylecain0@gmail.com>
2023-07-03 09:17:39 -0400

    successfully tested /logs endpoint

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, jsonify
+import os
 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def hello():
     return "Hello, World!"
+
+
+@app.route("/logs", methods=["GET"])
+def get_logs():
+    filename = "syslog"
+    filepath = "var/log/" + filename
+    if not os.path.isfile(filepath):
+        return "File not found", 404
+
+    with open(filepath, "r") as f:
+        lines = f.readlines()
+
+    return jsonify(lines)
 
 
 if __name__ == "__main__":
