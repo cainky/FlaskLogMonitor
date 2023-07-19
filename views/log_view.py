3d6987c9d723c8531b8502cda7c680a41480caae
cainky <kylecain0@gmail.com>
2023-07-19 13:23:18 -0400

    add secure filename for codeql

--- views/log_view.py
+++ views/log_view.py
@@ -11,18 +11,21 @@ from app_utils import (
 )
 from flask import current_app as app
 
+from werkzeug.utils import secure_filename
 
 def get_logs() -> tuple[Response, HTTPStatus]:
     log_directory = app.config["LOG_DIRECTORY"]
     filename = request.args.get("filename", default="syslog", type=str)
     search_term = request.args.get("term", default="", type=str)
     lines_limit = request.args.get("limit", default=50, type=int)
-
+    
     if not is_filename_valid(filename):
         return (
             jsonify(error=ErrorMessage.INVALID_FILENAME.value),
             HTTPStatus.BAD_REQUEST,
         )
+    filename = secure_filename(filename)
+
     if not is_search_term_valid(search_term):
         return (
             jsonify(error=ErrorMessage.LONG_SEARCH_TERM.value),
